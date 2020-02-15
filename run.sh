718930590a7e5257fd707a46ad3920e061e5b4af
Logan Donley <logan.donley@gmail.com>
2020-02-15 12:06:42 -0500

    UPdating run.sh

--- run.sh
+++ run.sh
@@ -4,3 +4,6 @@ sudo apt update
 sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
+git clone https://github.com/logandonley/laptop-setup.git
+cd laptop-setup
+ansible-playbook -i hosts setup_laptop.yaml
