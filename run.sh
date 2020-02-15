c425a0ec1bb5591d8e23e366887691d5359db56b
Logan Donley <logan.donley@gmail.com>
2020-02-15 13:09:14 -0500

    Adding curl

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible git -y
+sudo apt install ansible git curl -y
 git clone https://github.com/logandonley/laptop-setup.git
 cd laptop-setup
 ansible-playbook -i hosts setup_laptop.yaml
